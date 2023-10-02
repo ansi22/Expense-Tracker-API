6523b953a1ce51d9ecbad7c23ba219901496e215
Ritika Verma <iansi@Ritikas-MacBook-Air.local>
2023-10-02 23:22:13 +0530

    added response status

--- src/main/java/in/ritika/expensetrackerapi/controller/ExpenseController.java
+++ src/main/java/in/ritika/expensetrackerapi/controller/ExpenseController.java
@@ -3,6 +3,7 @@ package in.ritika.expensetrackerapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.ritika.expensetrackerapi.entity.Expense;
@@ -21,26 +23,31 @@ public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
     
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping("/expenses")
     public List<Expense> getAllExpenses() {
         return expenseService.getAllExpenses();
     }
 
+    @ResponseStatus(value = HttpStatus.OK)
     @GetMapping("/expenses/{id}")
     public Expense getExpenseById(@PathVariable Long id) {
         return expenseService.getExpenseById(id);
     }
 
+    @ResponseStatus(value=HttpStatus.NO_CONTENT)
     @DeleteMapping("/expenses")
     public void deleteExpenseById(@RequestParam Long id) {
         expenseService.deleteExpenseById(id);
     }
 
+    @ResponseStatus(value=HttpStatus.CREATED)
     @PostMapping("/expenses")
     public Expense saveExpenseDetails(@RequestBody Expense expense) {
         return expenseService.saveExpenseDetails(expense);
     }
 
+    @ResponseStatus(value = HttpStatus.OK)
     @PutMapping("/expenses/{id}")
     public Expense updateExpenseDetails(@RequestBody Expense expense, @PathVariable Long id) {
         return expenseService.updateExpenseDetails(id, expense);
